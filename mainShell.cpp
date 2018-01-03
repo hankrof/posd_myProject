898028b2d07e5d3e28c8622a1329459c06639ec6
hankrof <hankrof@gmail.com>
2018-01-03 17:35:51 +0800

    Fixed there is no dealing with empty input.

--- mainShell.cpp
+++ mainShell.cpp
@@ -6,27 +6,37 @@ int main(int argc, char **argv)
   PrologShell shell;
   while(1)
   {
-    char c;
+    bool exe = false;
     std::string input;
+    std::string executedString;
     input.clear();
     cout << "?- ";
-    while((c = getchar()))
+    while(!exe)
     {
-      if(!isspace(c))
-        input.push_back(c);
-      if(c == '\n')
-        cout << "|   ";
-      if(c == '.')
+      getline(cin,input);
+      if(input.empty())
       {
-        getchar(); //pop the final '\n'
-        break;
+        cout << "?- ";
+        continue;
       }
+      for(char c : input)
+      {
+        if(!isspace(c))
+          executedString.push_back(c);
+        if(c == '.')
+        {
+          exe = true;
+          break;
+        }
+      }
+      if(!exe)
+        cout << "|   ";
     }
     if(input == "halt.")
       break;
     try
     {
-      std::cout << shell.execute(input) << std::endl;
+      std::cout << shell.execute(executedString) << std::endl;
     }
     catch(std::string s)
     {
